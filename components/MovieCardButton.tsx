aafcf0eff019cd7108e77fde840c8232b27b071f
Luis <simosa37@gmail.com>
2025-03-16 10:12:20 -0300

    Update MovieCardButton to manage local state for movie presence in database

--- components/MovieCardButton.tsx
+++ components/MovieCardButton.tsx
@@ -2,26 +2,31 @@
 
 import { addMovie, removeMovie } from "@/lib/actions";
 import { TMDBMovie } from "@/types";
+import { useState } from "react";
 
 const MovieCardButton = ({
   movie,
   isMovieInDb,
 }: {
   movie: TMDBMovie;
-  isMovieInDb: boolean;
+  isMovieInDb: number | boolean;
 }) => {
+  const [isInDb, setIsInDb] = useState<boolean>(!!isMovieInDb);
+
   return (
     <button
       className="bg-transparent border border-zinc-700 hover:border-zinc-600 text-white text-sm py-2 px-4 rounded-md transition-colors flex-1 cursor-pointer"
       onClick={() => {
         if (isMovieInDb) {
-          removeMovie(movie.id);
+          removeMovie(isMovieInDb as number);
+          setIsInDb(false);
         } else {
           addMovie(movie as TMDBMovie);
+          setIsInDb(true);
         }
       }}
     >
-      {isMovieInDb ? "Remove from List" : "Add to List"}
+      {isInDb ? "Remove from List" : "Add to List"}
     </button>
   );
 };
